0eb1b25ac19d0a91e204573885f4e5515946cdfb
Aaron_Y <amyerke@uncc.edu>
2017-08-30 15:48:21 -0400

    No EC yet

--- src/lab2/RandomSequences.java
+++ src/lab2/RandomSequences.java
@@ -1,5 +1,56 @@
 package lab2;
 
-public class RandomSequences {
+import java.util.Random;
+
+public class RandomSequences 
+{
+	public static void main (String[] args) 
+	{
+		String nuc = null;
+		
+		int aaaCounter = 0;
+		
+		int aCounter = 0;
+		
+		Random random = new Random();
+		
+		for(int i = 0; i<1000; i++)
+		{
+			int ran = random.nextInt(4);
+			
+			switch (ran)
+			{
+			case 0: nuc = "A";	
+				break;
+			case 1: nuc = "C";
+				break;
+			case 2: nuc = "G";
+				break;
+			case 3: nuc = "T";
+				break;
+			}
+			
+			if (nuc == "A")
+			{
+				aCounter += 1;
+			} 
+			else
+			{
+				aCounter = 0;
+			}
+			
+			if (aCounter == 3)
+			{
+				aaaCounter += 1;
+				aCounter = 0;
+			}
+			
+			System.out.println(nuc);
+		}
+		
+		System.out.println("AAA is expected to appear 15.65 times (0.25*0.25*0.25*1000).  In this program, it appeared " + aaaCounter + " times.");
+
+	}
+	
 
 }

8f2b5722b706298bac3425574b646d3b5b05e701
Aaron_Y <amyerke@uncc.edu>
2017-11-04 11:36:02 -0400

    Lab 7 is complete

--- src/lab7/GameGUI.java
+++ src/lab7/GameGUI.java
@@ -1,29 +1,33 @@
+/**
+ * Author: Aaron Y
+ * lab 7 for Adv progr
+ * This is a GUI for the amino acid quiz.
+ */
+
 package lab7;
 
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Calendar;
 import java.util.Random;
 
 public class GameGUI extends JFrame
 {
 	private static final long serialVersionUID = 1L;
-	private JTextField aTextField = new JTextField();
-	private JButton startGame = new JButton("Start the game");
-	private JButton cancel = new JButton("Cancel");
-	private JTextField answerBox = new JTextField(20);
-	private JTextField counter = new JTextField();
-	private JTextField timer = new JTextField();
-	private JTextField feedback = new JTextField();
+	private static JTextField aTextField = new JTextField();
+	private static JButton startGame = new JButton("Start the game");
+	private static JButton cancel = new JButton("Cancel");
+	private static JTextField answerBox = new JTextField(20);
+	private static JTextField timerDisplay = new JTextField();
 	private static final Random random = new Random();
-	private volatile boolean Continue = true;
+	private volatile static boolean Continue = true;
 	String aminoFull;
 	String aminoLet;
 	String input = null;
-	int correct = 0;
+	static int correct = 0;
 	int count = 0;
+	private static final int SECONDS = 10;
 	
 	
 	public static String[] SHORT_NAMES = 
@@ -53,7 +57,8 @@ public class GameGUI extends JFrame
 		setSize(400,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(timer, BorderLayout.EAST);
+		getContentPane().add(timerDisplay, BorderLayout.EAST);
+		timerDisplay.setText("Timer");
 		getContentPane().add(startGame, BorderLayout.NORTH);
 		getContentPane().add(cancel, BorderLayout.WEST);
 		getContentPane().add(aTextField, BorderLayout.CENTER);
@@ -67,6 +72,7 @@ public class GameGUI extends JFrame
 			public void actionPerformed(ActionEvent e){
 				String input = answerBox.getText().toUpperCase();
 				
+				if (Continue == true) {
 				if (input.equals(aminoLet)) 				
 				{
 					correct += 1;
@@ -81,14 +87,19 @@ public class GameGUI extends JFrame
 				
 				makeQuestion();
 				}
+			}
 			});
 		cancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e){
 				answerBox.setEditable(false);
 				startGame.setEnabled(true);
 				cancel.setEnabled(false);
-				updateTextField("Thanks for playing!  You got ".concat(Integer.valueOf(correct).toString().concat(" correct!")));
+				updateTextField("Thanks for playing!  You correctly got ".concat(Integer.valueOf(correct).toString().concat(" correct!")));
 				updateStartGame("Start the game");
+				Continue = false;
+				try{  
+				Timer.interrupt();  
+				}catch(Exception e1){System.out.println("Exception handled "+e1);}  
 				
 			}
 		});
@@ -96,40 +107,35 @@ public class GameGUI extends JFrame
 		setVisible(true);
 	}
 	
-	private void updateTimer(String message) 
-	{
-		timer.setText(message);
-		validate();
-	}
-		
-	
-	private void updateTextField(String message)
-	{
-		aTextField.setText(message);
-		validate();
-	}
-	
-	private void updateStartGame(String message)
-	{
-		startGame.setText(message);
-		validate();
-	}
-	
-	private void updateFeedback(String message)
-	{
-		feedback.setText(message);
-		validate();
-	}
-	
 	private class StartGameListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent arg0)
 		{
+			(new Thread(new Timer())).start();
 			startGame.setEnabled(false);
 			answerBox.setEnabled(true);
 			cancel.setEnabled(true);
+			Continue = true;
+			correct = 0;		
+			count = 0;
 			runGame();
 		}
+	}
+	
+	private static void updateTimer(String message) 
+	{
+		timerDisplay.setText(message);
+	}
+		
+	
+	private static void updateTextField(String message)
+	{
+		aTextField.setText(message);
+	}
+	
+	private static void updateStartGame(String message)
+	{
+		startGame.setText(message);
 	}
 	
 	public void makeQuestion() {
@@ -144,43 +150,44 @@ public class GameGUI extends JFrame
 		updateTextField("What is the single letter code for " + aminoFull + "?");
 		System.out.println(aminoFull);
 		}else {
-			System.out.println("ovr");
+			System.out.println("over");
 		}
-		
-		
-		
-		
 	}
 	
 	public void runGame()
 	{
-		//instantiate timer and get ending time
-		Calendar cal = Calendar.getInstance();
-		long endTime = cal.getTimeInMillis() + 30000;
-		long now =  cal.getTimeInMillis();
-		
-		correct = 0;		
-		count = 0;
-		
-		//boolean continue = True
-		//while (now < endTime)
-		{		
-			//System.out.println(amino);
-			
-			
-			
-			
-			//int amino = random.nextInt(20);
-			//updateTextField("What is the single letter code for " + FULL_NAMES[amino] + "?");
-			makeQuestion();
-			
-			//find current time (now)
-			cal = Calendar.getInstance();
-			now =  cal.getTimeInMillis();
-			}
-	//	updateTextField("Time's up! You answered " + correct + " out of " + count + " correctly.");
+		makeQuestion();
+		answerBox.setEditable(true);
 	}
+	
+	private static class Timer implements Runnable
+	{	
+		@Override
+		public void run() {
+		try{		
+			for (int i = SECONDS; i > 0; i--) {
+				System.out.println("sleeping");
+				Thread.sleep(1000);
+				updateTimer(Integer.valueOf(i).toString());
+			}
+			updateTextField("Time's up! You correctly got ".concat(Integer.valueOf(correct).toString().concat(" correct!")));
+			answerBox.setEditable(false);
+			startGame.setEnabled(true);
+			cancel.setEnabled(false);
+			Continue = false;
+			updateStartGame("Start the game");
+			}catch(InterruptedException e){  
+			throw new RuntimeException("Thread interrupted..."+e);  
+			}
+			
+		}
 
+		public static void interrupt() {
+			System.out.println("interrupted");
+			
+		}  
+	}
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(
 				new Runnable() {
